b8840669ff35fdc75880ddab2341ce73f5fd3e03
tridc <dctri211997@gmail.com>
2022-08-27 11:43:30 +0700

    update

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,17 +1,19 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { Todo } from './src/entities/todo.entity';
-import { todo1661268343148 } from './src/migrations/1661268343148-todo'
+import { todo1661268343148 } from './src/migrations/1661268343148-todo';
 
-export const connectionSource = new DataSource({
+export const dataSource = {
   migrationsTableName: 'migrations',
   type: 'mysql',
   host: 'localhost',
-  port: 3304,
+  port: 3306,
   username: 'root',
   password: '123456',
   database: 'nestexample',
   logging: true,
   synchronize: false,
   entities: [Todo],
-  migrations: [todo1661268343148],
-} as DataSourceOptions);
+  migrations: [todo1661268343148]
+};
+
+export const connectionSource = new DataSource(dataSource as DataSourceOptions);
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,27 +3,19 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './modules/todo/todo.module';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
-import { Todo } from './entities/todo.entity';
-
-console.log(__dirname + 'src/entities/*.entity.ts');
-
-
+import { dataSource } from '../ormconfig';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthService } from './modules/auth/auth.service';
+import { UsersModule } from './modules/users/users.module';
+import { UsersService } from './modules/users/users.service';
 @Module({
   imports: [
-    TodoModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3304,
-      username: 'root',
-      password: '123456',
-      database: 'nestexample',
-      synchronize: true,
-      // autoLoadEntities: true,
-      entities: [Todo],
-      migrations: [__dirname + 'src/migrations/*{.ts}'],
-      migrationsTableName: 'migrations'
+      ...dataSource,
     }),
+    AuthModule,
+    TodoModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [
@@ -31,6 +23,8 @@ console.log(__dirname + 'src/entities/*.entity.ts');
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
     },
+    AuthService,
+    UsersService,
   ],
 })
 export class AppModule {}
